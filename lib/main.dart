aa0ed4c3b66ed89a7ada5067482c5a29d02cf4a7
Andresqr19 <andre3s.sqr@gmail.com>
2020-02-27 09:09:01 -0300

    continue....

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Login',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
